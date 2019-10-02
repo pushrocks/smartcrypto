bd94115cb76fc1a0dcd1ffaf1b2193ac88ee96ea
Phil Kunz <gitlab@philkunz.com>
2019-10-02 19:13:45 +0200

    fix(core): update

--- ts/index.ts
+++ ts/index.ts
@@ -2,3 +2,9 @@ export * from './smartcrypto.classes.smartcrypto';
 export * from './smartcrypto.classes.keypair';
 export * from './smartcrypto.classes.privatekey';
 export * from './smartcrypto.classes.publickey';
+
+import { nodeForge } from './smartcrypto.plugins';
+
+export {
+  nodeForge
+};
